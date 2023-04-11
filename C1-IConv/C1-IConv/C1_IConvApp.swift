f1285a7ec957e267d59525981aaafefc8d81d4ac
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-04-11 20:48:38 -0300

    iconv initial commit

---
+++ C1-IConv/C1-IConv/C1_IConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  C1_IConvApp.swift
+//  C1-IConv
+//
+//  Created by Gabriel on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct C1_IConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
