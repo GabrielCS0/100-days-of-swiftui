9d747fbbe00b13d6f468e833d167f458d4f11c1f
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-05-05 20:06:28 -0300

    c2-rps initial commit

---
+++ C2-RPS/C2-RPS/C2_RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2_RPSApp.swift
+//  C2-RPS
+//
+//  Created by Gabriel on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct C2_RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C2-RPS/C2-RPS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C2-RPS
+//
+//  Created by Gabriel on 05/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
