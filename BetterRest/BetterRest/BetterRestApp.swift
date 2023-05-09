6cd667e607f5ba73b0f6fa6e20450e55ff192ee8
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-05-09 10:34:43 -0300

    betterrest initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gabriel on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
