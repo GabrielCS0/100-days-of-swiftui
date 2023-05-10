320a1770b32090f5c65420c61d76452f7990d4ae
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-05-10 09:59:10 -0300

    wordscramble initial commit

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gabriel on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
