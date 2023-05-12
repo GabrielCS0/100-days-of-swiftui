5fe1fa9e611c6b7c49929266483e82e3c350e6ca
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-05-12 18:18:26 -0300

    imultiply initial commit

---
+++ C3-IMultiply/C3-IMultiply/C3_IMultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  C3_IMultiplyApp.swift
+//  C3-IMultiply
+//
+//  Created by Gabriel on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct C3_IMultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
