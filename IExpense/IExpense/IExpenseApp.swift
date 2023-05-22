5f83974d5b6e2ba21f7292919d89e2ac268caa60
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-05-22 14:05:12 -0300

    iexpense initial commit

---
+++ IExpense/IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by Gabriel on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
