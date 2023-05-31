c10d7044416afa60296b98e74af73e1cbe5da08e
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-05-31 11:40:43 -0300

    myhabittracking initial commit

---
+++ MyHabitTracking/MyHabitTracking/MyHabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitTrackingApp.swift
+//  MyHabitTracking
+//
+//  Created by Gabriel on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
