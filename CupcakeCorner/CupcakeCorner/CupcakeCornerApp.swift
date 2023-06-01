5acba7e4b5692bff532ac90ccc0ad469b2163f73
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-01 14:41:56 -0300

    cupcakecorner initial commit

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Gabriel on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
