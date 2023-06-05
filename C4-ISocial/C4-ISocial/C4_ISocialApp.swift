4000dc19d33f2e50168330bacba794085278be46
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-05 11:44:54 -0300

    isocial initial commit

---
+++ C4-ISocial/C4-ISocial/C4_ISocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4_ISocialApp.swift
+//  C4-ISocial
+//
+//  Created by Gabriel on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct C4_ISocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
