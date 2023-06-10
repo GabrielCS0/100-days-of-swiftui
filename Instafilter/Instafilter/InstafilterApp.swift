b4371e28d026acd9be3c911d5499fd0ea282e85a
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-10 16:38:40 -0300

    instafilter initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Gabriel on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
