4d62b72a8c2f14bcd6a583b95b9f6ecedbaf97f5
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-13 10:35:42 -0300

    bucketlist initial commit

---
+++ Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Gabriel on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
