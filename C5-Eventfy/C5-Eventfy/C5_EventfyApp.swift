64bdb4f4a457670fb4cf2e968746e171052b9474
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-15 15:08:32 -0300

    eventfy initial commit

---
+++ C5-Eventfy/C5-Eventfy/C5_EventfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  C5_EventfyApp.swift
+//  C5-Eventfy
+//
+//  Created by Gabriel on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct C5_EventfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
