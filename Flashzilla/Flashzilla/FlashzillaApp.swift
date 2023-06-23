b1ba5649f8b00d73ec805ab1f9799c422839adab
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-23 12:01:36 -0300

    flashzilla initial commit

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Gabriel on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
