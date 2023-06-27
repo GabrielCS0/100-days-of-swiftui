e2b3244f5cff2d07e5f4cb27e9c683faa894d175
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-27 12:23:28 -0300

    rolldice initial commit

---
+++ RollDice/RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Gabriel on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
