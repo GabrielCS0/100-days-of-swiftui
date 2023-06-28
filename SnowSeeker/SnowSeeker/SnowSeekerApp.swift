2170825b14212ca22190e74551124481449b2592
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-06-28 15:29:55 -0300

    snowseeker initial commit

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Gabriel on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
